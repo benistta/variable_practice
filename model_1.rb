6443195616456011b959787d045a28d7bd57f627
Becky Nisttahuz <benistta@hotmail.com>
2021-10-19 10:00:59 -0600

    Add hashes

--- model_1.rb
+++ model_1.rb
@@ -9,3 +9,11 @@ flota1 = 5.3
 is_cold = true
 is_sunny = false
 is_boolean = true
+
+array1 = ["1,2,3"]
+modules = [1,2,3,4]
+favorite_vegetables = [carrots, peppers, spinach]
+
+homework = {Mod0: 1, Mod1: 3, Mod2: 2}
+sessions_homework_Mod0 = {session1: 3, session2: 2, session3: 2, session4: 1, session5: 0}
+grades = {last_name: 'nisttahuz', score: '10' }
